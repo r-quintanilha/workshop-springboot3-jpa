5206b714d73f7124a4f29eeb1fe56404adcaebc4
Ricardo Quintanilha <r.quintanilha01@gmail.com>
2024-08-27 18:26:34 -0300

    Project created

---
+++ src/test/java/com/rqtech/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rqtech.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
