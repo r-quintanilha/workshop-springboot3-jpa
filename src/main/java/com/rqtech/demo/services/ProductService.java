ca916342402a9e0c994dab7422f560c292dad265
Ricardo Quintanilha <r.quintanilha01@gmail.com>
2024-08-29 17:08:51 -0300

    Product

---
+++ src/main/java/com/rqtech/demo/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.rqtech.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rqtech.demo.entities.Product;
+import com.rqtech.demo.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
