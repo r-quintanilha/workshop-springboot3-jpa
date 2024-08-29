c95b2a9b0de5c9491501bcc7e76efb6fc1b40d5a
Ricardo Quintanilha <r.quintanilha01@gmail.com>
2024-08-29 16:08:49 -0300

    Category

--- src/main/java/com/rqtech/demo/config/TestConfig.java
+++ src/main/java/com/rqtech/demo/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.rqtech.demo.entities.Category;
 import com.rqtech.demo.entities.Order;
 import com.rqtech.demo.entities.User;
 import com.rqtech.demo.entities.enums.OrderStatus;
+import com.rqtech.demo.repositories.CategoryRepository;
 import com.rqtech.demo.repositories.OrderRepository;
 import com.rqtech.demo.repositories.UserRepository;
 
@@ -23,9 +25,17 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
